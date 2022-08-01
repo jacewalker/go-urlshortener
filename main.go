c465330db06408c9d52e6022d85bcef865f7bc73
jc <jc+git@jcwlkr.io>
2022-08-01 11:39:54 +0000

    Database saves, retrieves and redirect working

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"math/rand"
 	"net/http"
 	"net/url"
+	"time"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -31,13 +32,28 @@ func main() {
 	router.POST("/shorten", func(c *gin.Context) {
 		webAddress := c.PostForm("url")
 		workingUrl := ShortUrl{ShortString: generateRandomString(), WebAddress: formatUrl(webAddress)}
-
-		fmt.Println("Short String: ", workingUrl.ShortString, ", Web Address: ", workingUrl.WebAddress)
 		saveToDatabase(workingUrl)
 
 		c.HTML(http.StatusOK, "shorten.tmpl", gin.H{
-			"title": "URL Shortener",
+			"title":  "URL Shortener",
+			"shorty": fmt.Sprintf("https://inker.ink/" + workingUrl.ShortString),
 		})
+	})
+
+	router.GET("/:shortString", func(c *gin.Context) {
+		short := c.Param("shortString")
+		if short != "" {
+			lookupAddress := lookupFromDatabase(short)
+			log.Println("Look up address:", lookupAddress)
+
+			if lookupAddress != "" {
+				c.Redirect(http.StatusTemporaryRedirect, lookupAddress)
+			} else {
+				c.HTML(http.StatusOK, "index.tmpl", gin.H{
+					"title": "URL Shortener",
+				})
+			}
+		}
 	})
 
 	router.Run(":8080")
@@ -58,42 +74,43 @@ func formatUrl(webAddress string) string {
 }
 
 func generateRandomString() string {
-	// Generates a random string of length 4
-	letters := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
-	b := make([]rune, 4)
-	for i := range b {
-		b[i] = letters[rand.Intn(len(letters))]
-	}
-	return string(b)
+	rand.Seed(time.Now().UnixNano())
+	b := make([]byte, 4)
+	rand.Read(b)
+	return fmt.Sprintf("%x", b)[:4]
 }
 
 func saveToDatabase(newUrl ShortUrl) {
-	dsn := fmt.Sprintf("host=localhost port=5432 sslmode=disable TimeZone=Australia/Melbourne")
+	dsn := fmt.Sprintf("host=172.17.0.3 port=5432 dbname=short_urls user=postgres password=postgres sslmode=disable TimeZone=Australia/Melbourne")
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
-	// Need to install Postgres container and configure connection details to it
-
-	fmt.Println("DB: ", db)
-	fmt.Println("Error: ", err)
-
-	createDatabaseCommand := fmt.Sprintf("CREATE DATABASE URLS")
-	db.Exec(createDatabaseCommand)
-
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Failed to connect to database.")
 	}
-	db.Create(&ShortUrl{
-		ShortString: newUrl.ShortString,
-		WebAddress:  newUrl.WebAddress,
-	})
+
+	db.AutoMigrate(&ShortUrl{})
+
+	// Check if the web address already exists in the database
+	var existingUrl ShortUrl
+	db.Where("web_address = ?", newUrl.WebAddress).First(&existingUrl)
+	if existingUrl.ShortString != "" {
+		fmt.Println("Web address already exists in the database.")
+	} else {
+		db.Create(&ShortUrl{ShortString: newUrl.ShortString, WebAddress: newUrl.WebAddress})
+	}
+
 }
 
-// func lookupFromDatabase(shortString string) string {
-// 	db, err := gorm.Open(postgres.Open("host=localhost user=postgres password=postgres dbname=GoUrlShort port=5432 sslmode=disable TimeZone=Australia/Melbourne"), &gorm.Config{})
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	var shortUrl string
-// 	db.Where("short_string = ?", shortString).First(&shortUrl)
-// 	return shortUrl
-// }
+func lookupFromDatabase(shortString string) string {
+	dsn := fmt.Sprintf("host=172.17.0.3 port=5432 dbname=short_urls user=postgres password=postgres sslmode=disable TimeZone=Australia/Melbourne")
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("Failed to connect to database.")
+	}
+
+	short := new(ShortUrl)
+	db.Where("short_string = ?", shortString).First(&short)
+
+	return short.WebAddress
+}
